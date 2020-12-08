3366dc22e68fff8cef96c95f519fb0b5169c7886
chenboshuo <chenboshuo.hi@163.com>
2020-12-08 14:43:25 +0800

    :bug: correct some bugs.

--- src/homework/review1.R
+++ src/homework/review1.R
@@ -30,9 +30,9 @@
 # ## 喝牛奶与感冒的相关性
 # 写$H_0$,$H_1$
 
-# -   $H_0$: 喝牛奶与感冒无相关关系
+# -   $H_0$: 喝牛奶与感冒无显著差别
 #
-# -   $H_1$: 喝牛奶与感冒有相关关系
+# -   $H_1$: 喝牛奶与感冒有显著差别
 #
 # [reference](https://www.bioinfo-scrounger.com/archives/603/)
 
@@ -45,6 +45,8 @@ people = c(210,312,170,85,223)
 bands = c(0,1,2,3,4)
 cor.test(people,bands)
 
+chisq.test(people)
+
 # `p-value = 0.5225` 接受原假设，即5种品牌爱好者有显著性差异
 
 # ## 使用ForestData数据，绘制森林地区各月份温度的箱线图
@@ -52,39 +54,51 @@ cor.test(people,bands)
 forest = read.table("ForestData.txt",header=TRUE)
 forest
 
-boxplot(forest$temp)
+# boxplot(forest$temp)
+boxplot(temp~month,data=forest,main='森林各地区温度箱线图')
 
 # ## 使用ForestData数据，绘制各月份温度的直方图和带正态分布曲线的直方图，核密度图
 
-# hist(forest$temp) # 好像必须加参数 freq = FALSE， 我也不知道为什么 
-hist(forest$temp,freq = FALSE)
-temp_mean = mean(forest$temp)
+# hist(forest$temp) # 好像必须加参数 freq = FALSE， 转换成频率，否则看不出来
+hist(forest$temp,xlab='温度',ylab='频率',main='森林地区温度分布直方图',freq = FALSE)
+temp_mean = mean(forest$temp,rm.na=TRUE)
 temp_std = sd(forest$temp)
 x = seq(from=min(forest$temp),to=max(forest$temp),length.out=length(forest$temp))
 curve(dnorm(x,temp_mean,temp_std),add=TRUE)
+# lines(density(forest$temp),lty=4,col=4)
 # curve(dnorm(),add=TRUE)
+
+# 核密度曲线
+plot(density(forest$temp))
 
 # ## 使用ReportCard数据，得到avScore的频数图，并画出其直方图
 
 rc = read.table('ReportCard.txt',header=TRUE)
+rc = na.omit(rc)
 rc
 
-barplot(table(rc$avScore))
+table(rc$avScore)
+
+barplot(table(rc$avScore),xlab='平均分等级',ylab='人数',ylim=c(0,25))
 
 hist(table(rc$avScore))
 
 # ## 使用ForestData，做出RH和Temp的散点图，并在图上添加线性拟合线
 
-plot(forest$RH,forest$Temp)
+# plot(forest$RH,forest$Temp)
+plot(RH~temp, data=forest)
 
-lm(forest$RH ~ forest$temp)
+line = lm(RH ~ temp,data=forest)
+line
 
-plot(forest$RH,forest$Temp)
-abline(lm(forest$RH ~ forest$temp))
+# plot(forest$RH,forest$Temp)
+plot(RH~temp,data=forest)
+# abline(lm(forest$RH ~ forest$temp))
+# m0 = lm(RH ~ temp,data=forest)
+# abline(line$cofficients,col='red')
+abline(line,col='red')
 
 cor.test(forest$RH, forest$temp)
-
-# 直线这么离谱是因为一开始没有拒绝原假设吧
 
 # ## 使用ForestData，做出temp，RH和wind的矩阵散点图，即直观显示多对数值型变量的相关性
 
